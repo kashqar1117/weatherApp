d21ea672cb5ba91a96e6f4d52f5e83557257d872
Kristina <kashqar1117@gmail.com>
2021-01-13 11:18:19 -0600

    saving

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 
 var relativeTime = moment().add(1, 'days').calendar()
+var searchHistory =[]
+
 
 //current weather data
 function runCall(queryURL){
+
     console.log(queryURL)  
     $.ajax({ url: queryURL, method: "GET"}).then(function (response) {
         (response.main);
 //Icon varaibles
       var icon = response.weather[0].icon
       var iconUrl =  "http://openweathermap.org/img/w/" + icon + ".png"
-
-
-       
+     
 //First Card Information
         $('.card-title').text(response.name)
         $('.date').text(moment().format("MMM Do YY"))
@@ -20,8 +21,7 @@ function runCall(queryURL){
         $('.humidity').text("Humidity: " + response.main.humidity + "%")
         $('#iconHolder').attr('<img src = "http://openweathermap.org/img/wn/10d@2x.png">')
         $('.windSpeed').text('Wind Speed: ' +response.wind.speed+ ' miles')
-        fiveDayForcast(response)
-        
+        fiveDayForcast(response)     
     });
       
 }
@@ -29,21 +29,16 @@ function runCall(queryURL){
 function fiveDayForcast(response){
     var long = response.coord.lon;
     var lat = response.coord.lat;
-
-    var queryTwoUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly&appid=c47c7993a34be35d985a7350c0baca7c`
+    var queryTwoUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly&appid=c47c7993a34be35d985a7350c0baca7c`;
    
    //second Call
     $.ajax({ url: queryTwoUrl, method: "GET"})
     .then(function (forcastData) {
         
         console.log(forcastData)
-        var tempPara = forcastData.daily[0].temp.day;
-    // var humidpara = forcastData.daily[i].humidity;
-    // var windPara = forcastData.daily[i].wind_speed;
 
         for (var i =0; i<5;i++){
     //create
-            var lineP = $('<div>')
             var newDiv = $('<div>')
 
 
@@ -57,15 +52,16 @@ function fiveDayForcast(response){
             
            
      //innerHtml
-    // newDiv.html('<p>'+forcastData.daily[i].temp.day+'</p>')
+    
             newDiv.html(
             `<h5>${forcastData.timezone}</h5>
-            <h6>${moment().add(1, 'days').calendar()}</h6>
+            <h6>${moment().add(1 +i, 'days').calendar()}</h6>
             <img src = "http://openweathermap.org/img/w/${forcastData.daily[i].weather[0].icon}.png">    
             <p> Temp: ${forcastData.daily[i].temp.day}</p>
             <p> Humidity: ${forcastData.daily[i].humidity} %</p>
             <p> Wind Speed: ${forcastData.daily[i].wind_speed} miles</p>
-            <p> UV: ${forcastData.daily[i].uvi}</p>`)         
+            <p> UV: ${forcastData.daily[i].uvi}</p>`
+            )         
            
         }
 
@@ -78,13 +74,25 @@ function fiveDayForcast(response){
     }
     
     $('#button-addon2').on('click', function(){
-        
+        $('.card-title').empty()
+        $('.date').empty()
+        $('.icon').empty()
+        $('.temp').empty()
+        $('.humidity').empty()
+        $('.windSpeed').empty()
+        $('.card2').empty()
         
         var cityInput = $('.form-control').val().trim()
-        console.log(cityInput)
+        searchHistory.push(cityInput)
+        localStorage.setItem('searchHistory' , searchHistory)
+        
+        
         queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=c47c7993a34be35d985a7350c0baca7c`
         
         runCall(queryURL)
+
+        $('.fiveDayForcast').removeClass('hide')
+        $('.card').removeClass('hide')
     })
     
     
