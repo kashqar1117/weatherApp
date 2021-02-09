3e2f34a0f8fa1bd840340bdc0ce942643269aad1
Kristina Al-ashqar <kristinaal-ashqar@Kristinas-MacBook-Air.local>
2021-02-09 17:51:45 -0600

    last save for now

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 
 var relativeTime = moment().add(1, 'days').calendar()
-var searchHistory =[]
-
 
 //current weather data
-function runCall(queryURL){
-
-    console.log(queryURL)  
+function runCall(cityInput){
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=c47c7993a34be35d985a7350c0baca7c`
+    
+    
     $.ajax({ url: queryURL, method: "GET"}).then(function (response) {
         (response.main);
-//Icon varaibles
-      var icon = response.weather[0].icon
-      var iconUrl =  "http://openweathermap.org/img/w/" + icon + ".png"
-     
-//First Card Information
+        //Icon varaibles
+        var icon = response.weather[0].icon
+        var iconUrl =  "http://openweathermap.org/img/w/" + icon + ".png"
+        //  console.log("forcast rewsponse" , response)
+        //First Card Information
         $('.card-title').text(response.name)
         $('.date').text(moment().format("MMM Do YY"))
         $('#wicon').attr('src', iconUrl);
@@ -21,49 +20,64 @@ function runCall(queryURL){
         $('.humidity').text("Humidity: " + response.main.humidity + "%")
         $('#iconHolder').attr('<img src = "http://openweathermap.org/img/wn/10d@2x.png">')
         $('.windSpeed').text('Wind Speed: ' +response.wind.speed+ ' miles')
-        fiveDayForcast(response)     
+        fiveDayForcast(cityInput)     
     });
-      
+    
 }
-    //Five day forcast information
-function fiveDayForcast(response){
-    var long = response.coord.lon;
-    var lat = response.coord.lat;
-    var queryTwoUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=hourly&appid=c47c7993a34be35d985a7350c0baca7c`;
+//Five day forcast information
+function fiveDayForcast(cityInput){
+    console.log('searching')
+    
+    
+    var queryTwoUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=c47c7993a34be35d985a7350c0baca7c`
    
    //second Call
     $.ajax({ url: queryTwoUrl, method: "GET"})
     .then(function (forcastData) {
         
-        console.log(forcastData)
+        console.log(forcastData.list)
 
-        for (var i =0; i<5;i++){
-    //create
-            var newDiv = $('<div>')
+        var list = forcastData.list;
 
-
-     //addclass
-            newDiv.addClass('border')
+        for (var i =0; i<list.length; i++){
 
 
-    //append to forcastDiv
-            $('.fiveDayForcast').append(newDiv)
-            
-            
-           
-     //innerHtml
-    
-            newDiv.html(
-            `<h5>${forcastData.timezone}</h5>
-            <h6>${moment().add(1 +i, 'days').calendar()}</h6>
-            <img src = "http://openweathermap.org/img/w/${forcastData.daily[i].weather[0].icon}.png">    
-            <p> Temp: ${forcastData.daily[i].temp.day}</p>
-            <p> Humidity: ${forcastData.daily[i].humidity} %</p>
-            <p> Wind Speed: ${forcastData.daily[i].wind_speed} miles</p>
-            <p> UV: ${forcastData.daily[i].uvi}</p>`
-            )         
-           
-        }
+            const datapoint = list[i]
+             //create
+             if(i%7 !==0 ||  i==0) {
+                 continue;
+             }else{
+
+                 var newDiv = $('<div>')
+                 
+                 
+                 //addclass
+                 
+                 
+                 
+                 //append to forcastDiv
+                 
+                 
+                 
+                 //innerHtml
+                 
+                 newDiv.html(
+                     `
+                     <div  class = "forcastFive p-3 m-2 border"
+                     <h5>${datapoint.dt_txt}</h5>
+                     <h6>${moment(datapoint.dt_txt).calendar()}</h6>
+                     <img src = "http://openweathermap.org/img/w/${datapoint.weather[0].icon}.png">    
+                     <p> Temp: ${datapoint.main.temp}</p>
+                     <p> Humidity: ${datapoint.main.humidity} %</p>
+                     <p> Wind Speed: ${datapoint.wind.speed} miles</p>
+                     
+                     </div>`
+                     
+                     )         
+                     
+                     $('.fiveDayForcast').append(newDiv)
+                    }
+                }
 
         
    
@@ -72,8 +86,40 @@ function fiveDayForcast(response){
     
      
     }
+
+    //  // <p> UV: ${forcastData.daily[i].uvi}</p>
+
+// updating history - updating local storage
+    function updateHistory(cityInput){
+        var searchHistory = JSON.parse(localStorage.getItem('weatherHistory')) || [];
+
+            //update
+
+        searchHistory.push(cityInput)
+        localStorage.setItem('weatherHistory', JSON.stringify(searchHistory))
+    }
+
+//rendering past city inputs into the dom
+    function renderHistory(){
+        var searchHistory = JSON.parse(localStorage.getItem('weatherHistory')) || [];
+
+        console.log(searchHistory.slice(11))
+        var weatherHistoryElement = $('#weatherHistory')
+
+
+        weatherHistoryElement.empty()
+        for(var i =0 ; i<searchHistory.length; i++){
+           var newBtn =  $('<li>').text(searchHistory[i])
+           weatherHistoryElement.prepend(newBtn)
+        }
+
+    }
+
+    renderHistory();
     
     $('#button-addon2').on('click', function(){
+        event.preventDefault()
+        
         $('.card-title').empty()
         $('.date').empty()
         $('.icon').empty()
@@ -83,13 +129,12 @@ function fiveDayForcast(response){
         $('.card2').empty()
         
         var cityInput = $('.form-control').val().trim()
-        searchHistory.push(cityInput)
-        localStorage.setItem('searchHistory' , searchHistory)
+      
+          
         
-        
-        queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=c47c7993a34be35d985a7350c0baca7c`
-        
-        runCall(queryURL)
+        runCall(cityInput)
+        updateHistory(cityInput)
+        renderHistory();
 
         $('.fiveDayForcast').removeClass('hide')
         $('.card').removeClass('hide')
